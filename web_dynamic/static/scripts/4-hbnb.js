2ff3667a9bb4146d311879a146d357f161631573
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-05 15:03:51 +0200

    added task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -60,6 +60,40 @@ $(document).ready(function () {
 		dataType: "json",
 
 	});
-	
-
+	$('button').click(function (err) {
+        err.preventDefault();
+        $('article').remove();
+        $.ajax({
+			url: 'http://0.0.0.0:5001/api/v1/places_search',
+			type: 'POST',
+			data: JSON.stringify({}),
+			contentType: 'application/json',
+			success: (data) => {
+					data.forEach((place) =>
+						$("section.places").append(
+							`<article>
+					<div class="title_box">
+					<h2>${place.name}</h2>
+					<div class="price_by_night">$${place.price_by_night}</div>
+					</div>
+					<div class="information">
+					<div class="max_guest">${place.max_guest} Guest${
+								place.max_guest !== 1 ? "s" : ""
+							}</div>
+					<div class="number_rooms">${place.number_rooms} Bedroom${
+								place.number_rooms !== 1 ? "s" : ""
+							}</div>
+					<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+								place.number_bathrooms !== 1 ? "s" : ""
+							}</div>
+					</div>
+					<div class="description">
+					${place.description}
+					</div>
+						</article>`
+						)
+					);
+			}
+		});
+	});
 });
